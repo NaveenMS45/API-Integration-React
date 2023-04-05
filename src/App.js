70382c3004077e6a319b1e7148e3e49d342c666d
NaveenMS45 <naveensampath2907@gmail.com>
2023-04-05 14:22:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
+      const jsonData = await response.json();
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        <div>
+          <h1>COVID-19 Stats in India</h1>
+          <p>Total cases: {data.data.summary.total}</p>
+          <p>Total deaths: {data.data.summary.deaths}</p>
+          <p>Total discharged: {data.data.summary.discharged}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <h2>Regional Information:</h2>
+          <ul>
+            {data.data.regional.map((item, index) => (
+              <li key={index}>
+                <p>State: {item.loc}</p>
+                <p>Confirmed cases: {item.confirmedCasesIndian}</p>
+                <p>Deaths: {item.deaths}</p>
+                <p>Recovered: {item.discharged}</p>
+              </li>
+            ))}
+          </ul>
     </div>
   );
 }
 
-export default App;
+export default App
